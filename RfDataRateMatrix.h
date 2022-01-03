86c3291c160cb840d0e6e49f53436ec3a7a6695a
JimmyLiu <jimmy021016@gmail.com>
2022-01-03 18:24:04 +0800

    Add two derived classes

---
+++ RfDataRateMatrix.h
@@ -0,0 +1,11 @@
+#ifndef RF_DATA_RATE_MATRIX_H
+#define RF_DATA_RATE_MATRIX_H
+#include "Matrix.h"
+
+class RfDataRateMatrix : public Matrix
+{
+public:
+    using Matrix::Matrix;
+};
+
+#endif
---
+++ VlcDataRateMatrix.h
@@ -0,0 +1,11 @@
+#ifndef VLC_DATA_RATE_MATRIX_H
+#define VLC_DATA_RATE_MATRIX_H
+#include "Matrix.h"
+
+class VlcDataRateMatrix : public Matrix
+{
+public:
+    using Matrix::Matrix;
+};
+
+#endif
