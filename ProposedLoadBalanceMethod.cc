bfa5118f3e586658a1b8f9e82f7a7c8a8183f066
JimmyLiu <jimmy021016@gmail.com>
2022-01-03 18:13:21 +0800

    Refactor the first part of this file

--- ProposedLoadBalanceMethod.cc
+++ ProposedLoadBalanceMethod.cc
@@ -2,39 +2,31 @@
 #include <map>
 
 #include "ProposedLoadBalanceMethod.h"
+#include "TDMAMatrix.h"
+#include "AssociationMatrix.h"
+#include "AvailableResourceMatrix.h"
 
 
 
 void ProposedLoadBalanceMethod::DoAllocateState0(int &state,
-                                                std::vector<std::vector<double>> &RF_DataRate_Matrix,
-                                                std::vector<std::vector<double>> &VLC_DataRate_Matrix,
-                                                std::vector<std::vector<int>> &AP_Association_Matrix,
-                                                std::vector<std::vector<double>> &Handover_Efficiency_Matrix,
-                                                std::vector<std::vector<double>> &TDMA_Matrix,
+                                                RfDataRateMatrix &RF_data_rate_matrix,
+                                                VlcDataRateMatrix &VLC_data_rate_matrix,
+                                                AssociationMatrix &AP_association_matrix,
+                                                HandoverEfficiencyMatrix &handover_efficiency_matrix,
+                                                TDMAMatrix &TDMA_matrix,
                                                 MyUeList &my_UE_list) 
 {
     //clear AP association matrix （新一輪的開始要重新算）
-    for (int i = 0; i < RF_AP_Num + VLC_AP_Num; i++)
-        for (int j = 0; j < UE_Num; j++)
-            AP_Association_Matrix[i][j] = 0;
+    AP_association_matrix.resetToZero();
 
     //clear TDMA matrix （新一輪的開始要重新算）
-    for (int i = 0; i < RF_AP_Num + VLC_AP_Num; i++)
-    {
-
-        //forall i ,ρ(i,0) = 1 代表每個AP目前都還沒被人連(故剩餘時間比例 = 1)
-        TDMA_Matrix[i][0] = 1;
-
-        //目前都還沒有分時間
-        for (int j = 1; j < UE_Num + 1; j++)
-            TDMA_Matrix[i][j] = 0;
-    }
+    TDMA_matrix.resetToOne();
 
     //並根據Required datarate做大到小排序
     //爲了讓demand大的先挑
-
     std::sort(my_UE_list.begin(), my_UE_list.end(), [](My_UE_Node a, My_UE_Node b)
          { return a.Get_Required_DataRate() > b.Get_Required_DataRate(); });
+
 
     ////////////////////////////////
     ////////////////////////////////
@@ -48,137 +40,38 @@ void ProposedLoadBalanceMethod::DoAllocateState0(int &state,
     //若ap有多餘資源則stage2再分配
 
     /* AP selection Phase */
-
     //照demand 大->小順序,給每個UE挑
     for (int u = 0; u < my_UE_list.getSize(); u++)
     {
 
         //Avaliable_Resource_Matrix:即我論文提到所謂"max A-Q"的A Matrix
-        std::vector<double> Avaliable_Resource_Matrix(RF_AP_Num + VLC_AP_Num);
+        AvailableResourceMatrix available_resource_matrix(2, std::max(RF_AP_Num, VLC_AP_Num));
 
         //每當一個UE挑完AP的後,A Matrix的情況都會變化，所以每次都要更新最新的情況
-        for (int i = 0; i < RF_AP_Num; i++)
-            Avaliable_Resource_Matrix[i] = TDMA_Matrix[i][0]; //copy RF AP部分
-        for (int i = 0; i < VLC_AP_Num; i++)
-            Avaliable_Resource_Matrix[i + RF_AP_Num] = TDMA_Matrix[i + RF_AP_Num][0]; //copy VLC AP部分
+        available_resource_matrix.update(TDMA_matrix);
 
         /** 計算A-Q 這裏我們用A matrix扣掉相對應的Q **/
         //其意義：如果UE u 連到AP i且分得恰可滿足demand的資源後, AP i還會剩下多少residual resource
         //Note : 注意sort過後的index u未必是原本的UE Node ID
-
-        //A-=Q
-        for (int i = 0; i < RF_AP_Num; i++)
-            Avaliable_Resource_Matrix[i] -= my_UE_list.getElement(u).Get_Required_DataRate() / RF_DataRate_Matrix[i][my_UE_list.getElement(u).GetID()];
-            
-        //A-=Q
-        for (int i = 0; i < VLC_AP_Num; i++)
-            Avaliable_Resource_Matrix[i + RF_AP_Num] -= my_UE_list.getElement(u).Get_Required_DataRate() / VLC_DataRate_Matrix[i][my_UE_list.getElement(u).GetID()];
-
+        available_resource_matrix.allocate(u, my_UE_list, RF_data_rate_matrix);
+        available_resource_matrix.allocate(u, my_UE_list, VLC_data_rate_matrix);
+    
         //接下來要選max A-Q的AP，因爲我剩的資源越多，越有機會服務更多人
-        //用Beta記錄max A-Q的AP
-        int Beta = 0;
+        double max_residual = 0;
+        max_residual = available_resource_matrix.findBestAP(u, RF_data_rate_matrix, VLC_data_rate_matrix, my_UE_list, TDMA_matrix, AP_association_matrix);
 
-        //max_residual記錄max A-Q的值
-        double max_residual = Avaliable_Resource_Matrix[0];
-
-        /* 從RF AP set找出max A-Q的RF AP 即論文的Eq 17 */
-
-        //Beta_w 記錄剩餘資源最大的RF AP
-        int Beta_w = 0;
-
-        //記錄該資源剩餘量
-        double max_RF_residual = Avaliable_Resource_Matrix[0];
-
-        //遍歷RF AP更新最佳選擇
-        for (int i = 0; i < RF_AP_Num; i++)
-            if (Avaliable_Resource_Matrix[i] > max_RF_residual)
-            {
-
-                Beta_w = i;
-
-                max_RF_residual = Avaliable_Resource_Matrix[i];
-            }
-
-        /* 從VLC AP set找出max A-Q的VLC AP 即論文的Eq 16 */
-
-        //記錄剩餘資源最大的VLC AP
-        int Beta_l = RF_AP_Num;
-
-        //記錄該資源剩餘量
-        double max_VLC_residual = Avaliable_Resource_Matrix[RF_AP_Num];
-
-        //遍歷VLC AP更新最佳選擇
-        for (int i = 0; i < VLC_AP_Num; i++)
-            if (Avaliable_Resource_Matrix[i + RF_AP_Num] > max_VLC_residual)
-            {
-
-                Beta_l = i + RF_AP_Num;
-
-                max_VLC_residual = Avaliable_Resource_Matrix[i + RF_AP_Num];
-            }
-
-        /*接下來要決定到底要連Beta_l還是Beta_w ： 看誰能提供的滿意度高就連誰，如果一樣大優先連lifi*/
-
-        //連到Beta_l可提供的throughput
-        double potential_throughput_of_Beta_l = TDMA_Matrix[Beta_l][0] * VLC_DataRate_Matrix[Beta_l - RF_AP_Num][my_UE_list.getElement(u).GetID()];
-
-        //連到Beta_l可提供的滿意度,上限爲1
-        double potential_satisfaction_of_Beta_l = potential_throughput_of_Beta_l / my_UE_list.getElement(u).Get_Required_DataRate();
-
-        if (potential_satisfaction_of_Beta_l > 1)
-            potential_satisfaction_of_Beta_l = 1;
-
-        //連到Beta_w可提供的throughput
-        double potential_throughput_of_Beta_w = TDMA_Matrix[Beta_w][0] * RF_DataRate_Matrix[Beta_w][my_UE_list.getElement(u).GetID()];
-
-        //連到Beta_w可提供的滿意度,上限爲1
-        double potential_satisfaction_of_Beta_w = potential_throughput_of_Beta_w / my_UE_list.getElement(u).Get_Required_DataRate();
-
-        if (potential_satisfaction_of_Beta_w > 1)
-            potential_satisfaction_of_Beta_w = 1;
-
-        //找能提供較大滿意度的人連
-        if (potential_satisfaction_of_Beta_l >= potential_satisfaction_of_Beta_w)
-        {
-
-            Beta = Beta_l;
-
-            max_residual = max_VLC_residual;
-        }
-        else
-        {
-
-            Beta = Beta_w;
-
-            max_residual = max_RF_residual;
-        }
-
-        //連到chosen ap
-        AP_Association_Matrix[Beta][my_UE_list.getElement(u).GetID()] = 1;
-
-        //確定AP selection結果，記錄到myUElist中
-        my_UE_list.getElement(u).Set_Now_Associated_AP(Beta);
 
         /* Resource Allocation Phase */
-
-        //先盡可能配置滿足其demand的資源給ue u
+        //先盡可能配置滿足其demand的資源給UE u
         //如果無法滿足demand，則chosen ap剩的全給ue u
-
-        //如果剩餘資源量爲負，則代表無法滿足ue u的demand.故剩下的資源全給ue u,導致分配後剩餘資源 = 0
+        
+        //如果剩餘資源量爲負，則代表無法滿足ue u的demand.故剩下的資源全給UE u,導致分配後剩餘資源 = 0
         if (max_residual < 0)
             max_residual = 0;
 
-        //這行的想法是 ρ(Beta,u) (要分配的資源量) =   ρ(Beta,0)(目前所剩資源量) - max_residual(分配後的剩餘資源量)
-        TDMA_Matrix[Beta][my_UE_list.getElement(u).GetID() + 1] = TDMA_Matrix[Beta][0] - max_residual;
-
-        //在myUElist[u]中更新分得時間比例
-        //Note : 此時的值未必是最終值，之後再做residual resource allocation時也許會有變化
-        my_UE_list.getElement(u).Set_Time_Fraction(TDMA_Matrix[Beta][my_UE_list.getElement(u).GetID() + 1]);
-
-        //更新ρ(Beta,0)(目前所剩資源量) ，即要再扣掉剛剛分配的量，
-        TDMA_Matrix[Beta][0] -= TDMA_Matrix[Beta][my_UE_list.getElement(u).GetID() + 1];
+        int curr_AP = my_UE_list.getElement(u).Get_Now_Associated_AP();
+        TDMA_matrix.allocateTimeFraction(u, curr_AP, max_residual, my_UE_list);
     }
-    
 }
 
 void ProposedLoadBalanceMethod::DoAllocateStateN(int &state,
@@ -367,6 +260,7 @@ void ProposedLoadBalanceMethod::DoAllocateStateN(int &state,
                             TDMA_Matrix,
                             my_UE_list);
 }
+
 
 void ResidualResourceAllocate(int &state,
                             std::vector<std::vector<double>> &RF_DataRate_Matrix,
--- ProposedLoadBalanceMethod.h
+++ ProposedLoadBalanceMethod.h
@@ -9,11 +9,11 @@ class ProposedLoadBalanceMethod : public LoadBalanceMethod
 {
 private:
     virtual void DoAllocateState0(int &state,
-                                std::vector<std::vector<double>> &RF_DataRate_Matrix,
-                                std::vector<std::vector<double>> &VLC_DataRate_Matrix,
-                                std::vector<std::vector<int>> &AP_Association_Matrix,
-                                std::vector<std::vector<double>> &Handover_Efficiency_Matrix,
-                                std::vector<std::vector<double>> &TDMA_Matrix,
+                                RfDataRateMatrix &RF_DataRate_Matrix,
+                                VlcDataRateMatrix &VLC_DataRate_Matrix,
+                                AssociationMatrix &AP_Association_Matrix,
+                                HandoverEfficiencyMatrix &Handover_Efficiency_Matrix,
+                                TDMAMatrix &TDMA_Matrix,
                                 MyUeList &my_UE_list);
     virtual void DoAllocateStateN(int &state,
                                 std::vector<std::vector<double>> &RF_DataRate_Matrix,
