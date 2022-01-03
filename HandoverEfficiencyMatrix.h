d9aab279430f99242c25be39e7c60c988b8179a8
JimmyLiu <jimmy021016@gmail.com>
2022-01-03 20:28:06 +0800

    Add a derived class "HandoverEfficiencyMatrix"

---
+++ HandoverEfficiencyMatrix.h
@@ -0,0 +1,12 @@
+#ifndef HANDOVER_EFFICIENCY_MATRIX_H
+#define HANDOVER_EFFICIENCY_MATRIX_H
+#include "Matrix.h"
+
+class HandoverEfficiencyMatrix : public Matrix
+{
+public:
+    using Matrix::Matrix;
+    
+};
+
+#endif
